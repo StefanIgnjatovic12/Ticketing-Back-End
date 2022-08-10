0b2d04d4fc3bf2e86515a338e29a079cc5da0dd5
Stefan <sper1234@gmail.com>
2022-08-10 19:24:59 +0200

    Update settings

--- Ticketing_system_and_issue_tracker/settings.py
+++ Ticketing_system_and_issue_tracker/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-ohuh^epickfm)$29v#v--5q^itekvt%a@sy$72flo*=52lzth&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['drf-react-ticketing-backend.herokuapp.com', 'https://drf-react-ticketing-frontend.herokuapp.com/']
-CORS_ALLOWED_ORIGINS = ['https://drf-react-ticketing-backend.herokuapp.com', 'https://drf-react-ticketing-frontend.herokuapp.com/']
+ALLOWED_HOSTS = ['drf-react-ticketing-backend.herokuapp.com', 'https://drf-react-ticketing-frontend.herokuapp.com']
+CORS_ALLOWED_ORIGINS = ['https://drf-react-ticketing-backend.herokuapp.com', 'https://drf-react-ticketing-frontend.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
