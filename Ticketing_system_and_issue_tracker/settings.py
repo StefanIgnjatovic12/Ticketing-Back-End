d30badca3021c1b1853070d1e9e0244cc93f6d27
Stefan <sper1234@gmail.com>
2022-09-04 19:37:10 +0200

    Refactor

--- Ticketing_system_and_issue_tracker/settings.py
+++ Ticketing_system_and_issue_tracker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ohuh^epickfm)$29v#v--5q^itekvt%a@sy$72flo*=52lzth&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['drf-react-ticketing-backend.herokuapp.com']
 # Application definition
